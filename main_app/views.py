590712d02230b1188034ed5bd7a7a647c085d3a9
009kings <009kings@gmail.com>
2019-02-19 11:35:31 -0800

    Initial commit and the whole darn project

---
+++ main_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Cat
+
+# Create your views here.
+def index(request):
+  cats = Cat.objects.all()
+  return render(request, 'index.html', { 'cats': cats })
